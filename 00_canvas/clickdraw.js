692326f819b3f1b8662671c514852a1b8ef00874
Vincent Chi <34114571+vchi90@users.noreply.github.com>
2019-01-30 14:20:01 -0500

    Update clickdraw.js

--- 00_canvas/clickdraw.js
+++ 00_canvas/clickdraw.js
@@ -1,6 +1,8 @@
 var c = document.getElementById("slate");
 var ctx = c.getContext("2d");
 
-document.getElementById("Clear").addEventListener('click', function() {
-  ctx.clearRect( 0, 0, canvas.width, canvas.height);
+var clear_button = document.getElementById("Clear");
+
+clear_button.addEventListener('click', function() {
+  ctx.clearRect( 0, 0, 600, canvas.600);
 })
