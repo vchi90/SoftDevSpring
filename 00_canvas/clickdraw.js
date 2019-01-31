9ffd855e97b4bca0ab05718350aa8ba7eef8cf85
Vincent Chi <vchi90@stuy.edu>
2019-01-30 22:57:31 -0500

    00_canvas

--- 00_canvas/clickdraw.js
+++ 00_canvas/clickdraw.js
@@ -1,8 +1,36 @@
 var c = document.getElementById("slate");
 var ctx = c.getContext("2d");
 
-var clear_button = document.getElementById("Clear");
+var clear = document.getElementById("clear");
 
-clear_button.addEventListener('click', function() {
-  ctx.clearRect( 0, 0, 600, canvas.600);
-})
+clear.addEventListener('click', function(){
+  ctx.clearRect( 0, 0, 600, 600);
+});
+
+c.addEventListener('click', draw);
+
+var boxOrDot = document.getElementById("draw");
+
+boxOrDot.addEventListener('click', function() {
+  if (boxOrDot.innerHTML == "Rectangle") {
+	  boxOrDot.innerHTML = "Dot";
+  }
+  else {
+	  boxOrDot.innerHTML = "Rectangle";
+  }
+});
+
+function draw(e) {
+  var x = e.clientX;
+  var y = e.clientY;
+  x = x - c.offsetLeft;
+  y = y - c.offsetTop;
+  if (boxOrDot.innerHTML == "Rectangle"){
+    ctx.fillRect( x, y, 50, 100);
+  }
+  else{
+    ctx.beginPath();
+    ctx.ellipse( x, y, 1, 1, 0, 0, 2 * Math.PI);
+    ctx.fill()
+  }
+};
