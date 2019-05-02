83c7aa0b3f6e2c4a24a2774bd37f252a0c3dca19
Vincent Chi <vchi90@stuy.edu>
2019-05-02 13:07:13 -0400

    qeg

--- 23_memoize/notes.py
+++ 23_memoize/notes.py
@@ -20,9 +20,9 @@ print(greet())
 def memoize(f):
     memo = {}
     def helper(x):
-        if (x not in memo.keys()):
+        if x not in memo:
             memo[x] = f(x)
-        memo[x]
+        return memo[x]
     return helper
 
 @memoize
